949920b39307019532087d9113865f53d9f5d8ec
Bill Tsui <billkuentsui@outlook.com>
2020-09-20 00:14:39 +0800

    完成基本数据类型的学习

---
+++ CharTest.java
@@ -0,0 +1,11 @@
+public class CharTest {
+    public static void main(String[] args) {
+        char c1 = 'a';
+        System.out.println("c1 = " + c1);
+        System.out.println("对应的编号是:" + (int)c1);
+
+        char c2 = 98;
+        System.out.println("c2 = " + c2);
+        System.out.println("对应的编号是:" + (int)c2);
+    }
+}
