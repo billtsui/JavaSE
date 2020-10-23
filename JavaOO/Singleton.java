cc6c5abc25cca7accf287d5538ba5854a6679baa
Bill Tsui <billkuentsui@outlook.com>
2020-10-23 23:30:18 +0800

    单例的两种实现模式

--- JavaOO/Singleton.java
+++ JavaOO/Singleton.java
@@ -2,12 +2,17 @@ package JavaOO;
 
 public class Singleton {
 
-    private static Singleton singleton = new Singleton();
+    // private static Singleton singleton = new Singleton(); 饿汉式
+    private static Singleton singleton = null; //懒汉式
 
     private Singleton(){
     }
 
     public static Singleton getInstance(){
+        // return singleton; 饿汉式
+        if(null == singleton){
+            singleton = new Singleton();
+        }
         return singleton;
     }
 }
