218cb586942b4a2832266ba149980aaac2c3e7cc
Bill Tsui <billkuentsui@outlook.com>
2020-11-15 12:46:49 +0800

    TreeSet使用比较器排序

--- src/person/billtsui/collection/TreeSetTest.java
+++ src/person/billtsui/collection/TreeSetTest.java
@@ -1,10 +1,12 @@
 package person.billtsui.collection;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 /**
  * 底层是红黑树实现，添加的元素会自然排序
  * 如果没有自然排序，那么要使用比较器排序
+ *
  * @author billtsui
  * @date 2020/11/15
  */
@@ -21,10 +23,16 @@ public class TreeSetTest {
         System.out.println(treeSet);
         System.out.println("-----------------");
 
-        TreeSet<Student> studentTreeset = new TreeSet<Student>();
-        studentTreeset.add(new Student("liubei",40));
-        studentTreeset.add(new Student("guanyu",35));
-        studentTreeset.add(new Student("zhangfei",30));
+        //使用比较器对象作为参数传给构造方法
+        TreeSet<Student> studentTreeset = new TreeSet<Student>(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        studentTreeset.add(new Student("liubei", 40));
+        studentTreeset.add(new Student("guanyu", 35));
+        studentTreeset.add(new Student("zhangfei", 30));
         System.out.println(studentTreeset);
 
     }
